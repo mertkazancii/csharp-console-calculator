262244abaa65690b3020fd28cc6b82d4f7b9471a
mertkazancii <mertkazanci01@outlook.com>
2025-04-05 11:45:21 -0400

    Calculator Priject

---
+++ Calculator/Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+Console.WriteLine("Entter first number: ");
+Double firtsNumber = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter second number: ");
+Double secondNumber = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Choose operation (+, -, *, /):");
+string operation = Console.ReadLine();
+
+double result = 0;
+
+switch(operation)
+{
+    case "+":
+        result = firtsNumber + secondNumber;
+        break;
+
+    case "-":
+        result = firtsNumber - secondNumber;
+        break;
+
+    case "*":
+        result = firtsNumber * secondNumber;
+        break;
+
+    case "/":
+        result = firtsNumber / secondNumber;
+        break;
+
+    default:
+        Console.WriteLine("invalid transaction");
+        break;
+}
+
+Console.WriteLine("Result: " + result);
+
---
+++ README.md.txt
@@ -0,0 +1,12 @@
+# C# Calculator
+
+This project is a simple calculator developed as a C# Console Application.
+
+## Features
+- Addition
+- Subtraction
+- Multiplication
+- Division
+
+## Usage
+Open and run the project with Visual Studio.
